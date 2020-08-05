699b4c5456bd93b788bfaed42f49cd3545ede611
Lucas Guimaraes <lucaslgr1206@gmail.com>
2020-08-05 10:03:23 -0300

    Comentado uma base_url para tirar o erro de redeclaracao

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // “powered by ecomplato carros”
-const BASE_URL = 'http://localhost/guarda-tarefa'; //TODO : Desenvolvimento
+// const BASE_URL = 'http://localhost/guarda-tarefa'; //TODO : Desenvolvimento
 const BASE_URL = 'https://querocriarsite.com/projetos/tarefas/'; //TODO : Produção
 const CACHE_NAME = 'guardatarefas_v0';
 const STATIC_FILES = [
